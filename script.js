f9ebda00506cf298fb01806de9ea4833bb9bf5cd
lhtn <lily.nguyen1129@gmail.com>
2022-10-03 22:58:33 -0400

    Add reset button

--- script.js
+++ script.js
@@ -15,9 +15,22 @@ makeGrid();
 
 
 // modify color of square when mouse is moved over the grid
-let changeColor = document.querySelector(".grid-container");
+const changeColor = document.querySelector(".grid-container");
 
 changeColor.addEventListener("mouseover", function (event) {
     event.target.style.backgroundColor = "darkgrey";
 });
 
+
+// send the user a popup asking for the number of squares per side for the new grid
+const resetButton = document.querySelector("button");
+resetButton.addEventListener("click", resetPage)
+
+function resetPage() {
+    let num = prompt("How many squares per side?");
+    if (num <= 100) {
+        console.log("Great!");
+    } else if (num > 100) {
+        console.log("Try again");
+    }
+}
