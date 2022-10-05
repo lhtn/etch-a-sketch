ae3bc23c13c536017258ac90d031107b4970d1b0
lhtn <lily.nguyen1129@gmail.com>
2022-10-04 23:16:01 -0400

    Add reset button

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// makes a grid background
-const gridContainer = document.querySelector(".grid-container");
+// makes a grid
 
-function makeGrid() {
-    for (let row = 0; row < 16; row++) {
-        for (let column = 0; column < 16; column++) {
+function makeGrid(num) {
+    const gridContainer = document.querySelector(".grid-container");
+    for (let row = 0; row < num; row++) {
+        for (let column = 0; column < num; column++) {
             const square = document.createElement("div");
             square.className = 'square';
             gridContainer.appendChild(square);
@@ -11,7 +11,7 @@ function makeGrid() {
     }
 }
 
-makeGrid();
+makeGrid(16);
 
 
 // modify color of square when mouse is moved over the grid
@@ -23,14 +23,14 @@ changeColor.addEventListener("mouseover", function (event) {
 
 
 // send the user a popup asking for the number of squares per side for the new grid
-const resetButton = document.querySelector("button");
-resetButton.addEventListener("click", resetPage)
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", resetPage);
 
 function resetPage() {
     let num = prompt("How many squares per side?");
-    if (num <= 100) {
-        console.log("Great!");
-    } else if (num > 100) {
-        console.log("Try again");
+    if (num >= 1 && num <= 100) {
+        alert("Great!")
+    } else {
+        alert("Try again");
     }
 }
