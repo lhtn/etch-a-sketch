77fb2b873599829b08c0b895cdfe5d0b03d4dbff
lhtn <lily.nguyen1129@gmail.com>
2022-10-10 00:16:08 -0400

    Create buttons to change color of grid

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const gridContainer = document.querySelector(".grid-container");
-const changeColor = document.querySelector(".grid-container");
-const resetButton = document.getElementById("reset-button");
+const defaultColors = document.getElementById("default-colors");
+const changeOriginalGrid = document.querySelector(".grid-container");
+const randomColors = document.getElementById("random-colors");
+const makeNewGrid = document.getElementById("new-grid");
 
 
 // makes a grid
@@ -18,20 +20,37 @@ function makeGrid(gridNumber) {
 makeGrid(16);
 
 
-// modify color of square when mouse is moved over the grid
-changeColor.addEventListener("mouseover", function (event) {
+// modify color of square between light and dark grey when mouse is moved over the grid
+changeOriginalGrid.addEventListener("mouseover", function (event) {
     event.target.style.backgroundColor = "darkgrey";
 });
 
 
+defaultColors.addEventListener("click", function (event) {
+    let defaultCol = document.querySelector(".grid-container");
+    defaultCol.addEventListener("mouseover", function (event) {
+        event.target.style.backgroundColor = "darkgrey";
+    })
+});
+
+
+// modify color of square by random color when mouse is moved over the grid
+randomColors.addEventListener("click", function (event) {
+    let random = document.querySelector(".grid-container");
+    random.addEventListener("mouseover", function (event) {
+        event.target.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    })
+})
+
+
 // send the user a popup asking for the number of squares per side for the new grid
-resetButton.addEventListener("click", () => {
-    let cell = document.querySelectorAll(".square");
-    for (let c = 0; c < cell.length; c++) {
-        gridContainer.removeChild(cell[c]);
-    }
+makeNewGrid.addEventListener("click", () => {
     let userInput = prompt("How many squares per side?");
     if (userInput > 1 && userInput <= 100) {
+        let cell = document.querySelectorAll(".square");
+        for (let c = 0; c < cell.length; c++) {
+            gridContainer.removeChild(cell[c]);
+        }
         makeGrid(userInput);
     } else {
         alert("Choose a number greater than 1 and less than 100");
