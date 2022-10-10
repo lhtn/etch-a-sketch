874cf8ea7038ac12e0acdf1166892ac42ab2abf2
lhtn <lily.nguyen1129@gmail.com>
2022-10-09 23:16:12 -0400

    Fix resetButton to ensure correct number of squares in grid is created

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const gridContainer = document.querySelector(".grid-container");
 const changeColor = document.querySelector(".grid-container");
-const cell = document.querySelectorAll(".square");
 const resetButton = document.getElementById("reset-button");
+
 
 // makes a grid
 function makeGrid(gridNumber) {
-    const grid = gridNumber * gridNumber;
+    let grid = gridNumber * gridNumber;
     for (let i = 0; i < grid; i++) {
         const square = document.createElement("div");
         square.className = 'square';
@@ -18,23 +18,22 @@ function makeGrid(gridNumber) {
 makeGrid(16);
 
 
-
 // modify color of square when mouse is moved over the grid
 changeColor.addEventListener("mouseover", function (event) {
     event.target.style.backgroundColor = "darkgrey";
 });
 
 
-
 // send the user a popup asking for the number of squares per side for the new grid
 resetButton.addEventListener("click", () => {
+    let cell = document.querySelectorAll(".square");
     for (let c = 0; c < cell.length; c++) {
         gridContainer.removeChild(cell[c]);
     }
     let userInput = prompt("How many squares per side?");
-    if (userInput >= 1 && userInput <= 100) {
+    if (userInput > 1 && userInput <= 100) {
         makeGrid(userInput);
     } else {
-        alert("Try again");
+        alert("Choose a number greater than 1 and less than 100");
     }
 });
